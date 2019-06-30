da89e3a707639d48136782c42a804155645628d2
Pranjal Ghimire <happyghimire7@gmail.com>
2019-06-30 18:32:27 +0545

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,117 +1,115 @@
 #include <iostream>
+#include <windows.h>
 #include <conio.h>
-
+#include <time.h>
 using namespace std;
 
-bool gameOver;
-const int height = 6;
-const int width = 100;
-const int gravity = 02;
-int x, y, score;
-enum eDirection{Stop=0, Move};
-eDirection dir;
-
-void Setup()
-   {
-	gameOver = false;
-	dir = Stop;
-	x = 0;
-	y = height / 2;
-	score = 0;
-   }
-
-void Draw()
-{
-	system("cls");
-	for (int i = 0; i < width; i++)
-	    {
-		cout << "#";
-	    }
-	cout << endl;
-
-
-	
-	for (int i = 0; i < height; i++)																						
-	{
-        for(int j = 0; j < width; j++ )
-		   {
-			if (j == 0 || j == width-1)
-				cout << "#";
-
-			else
-				cout << " ";
-
-			if (i == y && j == x)
-			{
-				cout << "B";
-			}
-
-		   }
-		cout << endl;
-	}
-
-	for (int i = 0; i < width; i++)
-	{
-		cout << "#";
-	}
-	cout << endl;
-
-	
-
-
-
-
-}
-
-void Input()
-{
-	if (_kbhit())
-	   {
-		switch (_getch())
-		{
-		    case ' ':
-				dir = Move;
-			break;
-
-			case 'p':
-				gameOver = true;
-
-				break;
-			default:
-				dir = Move;
-			break;
-
-
-		}
-
-
-
-	   }
-}
-
-void Logic()
-{
-	switch (dir)
-	{
-	
-	case Move:
-			y = y-gravity;
-		break;
-	default:
-		break;
-	}
-	
-
-}
-
 int main()
-   {
-	Setup();
-	while (!gameOver)
-	     {
-		Draw();
-		Input();
-		Logic();
-	     }
+{
+	srand(time(0));//to make the program randomize
+	char mv;
+	char restart = 'y';
+	int highscore = 0;
+	while (restart == 'y')
+       {
+		int score = 0;
+		bool wasit = true;
+		int x = 9;
+		int yAxis[5]; // the y-axis of obstacle
+		int xAxis[5]; // the x-axis of obstacle
+		xAxis[0] = 9;
+		xAxis[1] = 18;
+		xAxis[2] = 27; // the starting x-axis of obstacle
+		for (int i = 0; i < 3; i++)
+		  {
+			yAxis[i] = (rand() - 1) % 12 + 2;
+		  }
+		string map[20][30];
+		for (int i = 0; i < 20; i++)
+		  {
+			for (int j = 0; j < 30; j++)
+			   {
+				if (i == 0 || i == 19 || j == 29 || j == 0)
+					map[i][j] = "# ";
+				else
+					map[i][j] = "  ";
+			   }
+		  }
+		while (wasit == true)
+		  {
+             for (int i = 0; i < 3; i++)
+			  {
+				for (int j = 1; j < 19; j++)
+				  {
+					map[j][xAxis[i]] = "  ";
+				  }
+			  }
+			for (int i = 0; i < 3; i++) //make the obstacle move from right to left
+			  {
+				xAxis[i]--;
+			  }
+			for (int i = 0; i < 3; i++) //make the obstacle back to right side if it touched left side
+			  {
+				for (int j = 1; j < 19; j++)
+				  {
+					map[j][xAxis[i]] = "* ";
+				  }
+			  }
+			for (int i = 0; i < 3; i++) //to make a hole at the obstacle
+			  {
+				map[yAxis[i]][xAxis[i]] = "  ";
+				map[yAxis[i] + 1][xAxis[i]] = "  ";
+				map[yAxis[i] + 2][xAxis[i]] = "  ";
+				map[yAxis[i] + 3][xAxis[i]] = "  ";
+				map[yAxis[i] + 4][xAxis[i]] = "  ";
+			  }
+			for (int i = 0; i < 3; i++)
+			  {
+				if (xAxis[i] == 1)
+				  {
+					xAxis[i] = 28;
+					yAxis[i] = (rand() - 1) % 12 + 2;
+				  }
+			  }
+			map[x][3] = "  ";
+			if (kbhit())
+			  {
+				mv = getch();
+				x = x - 3;
+			  }
+			x++;
+			if (x < 1)
+				x = 1;
+			for (int i = 0; i < 3; i++)
+			  {
+				if (xAxis[i] == 3)
+					if (map[x][3] == "  ")
+						score = score + 1;
+				if (map[x][3] == "* ")
+					wasit = false;
+			  }
+			map[x][3] = "B>";
+			for (int i = 1; i < 19; i++)
+			  {
+				map[i][1] = "  ";
+			  }
+			for (int i = 0; i < 20; i++)
+			  {
+				for (int j = 0; j < 30; j++)
+				  {
+					cout << map[i][j];
+				  }
+				cout << endl;
+			  }
+			cout << "Score = " << score;
+            Sleep(96);
+			system("cls");
+		}
+		cout << "Score = " << score << endl;
+		if (highscore < score)
+			highscore = score;
+		cout << "Highscore= " << highscore << endl;
+		cout << "restart?(y/n) = "; cin >> restart;
+	}
 	return 0;
-   }
+}
